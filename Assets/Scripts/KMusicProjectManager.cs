0b0c200cd2cfa72989a4b4cfe5bac6d31d77b689
Tekknobot <thrillatron@hotmail.com>
2026-03-10 15:53:37 -0400

    Keep screen awake.

--- Assets/Scripts/KMusicProjectManager.cs
+++ Assets/Scripts/KMusicProjectManager.cs
@@ -56,6 +56,8 @@ public sealed class KMusicProjectManager : MonoBehaviour
 
     private void Awake()
     {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep; // Screentimeout: NEVER
+
         if (!doc) doc = GetComponent<UIDocument>();
         CurrentSlot = Mathf.Clamp(ProjectPrefs.GetGlobalInt(PrefKey_CurrentSlot, 0), 0, 999);
         ProjectPrefs.SetActiveSlot(CurrentSlot);
